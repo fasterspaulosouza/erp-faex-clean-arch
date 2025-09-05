0ef1db81f39d2c009da8c4820446a7aaea0fe84e
Paulo Souza <paulos@portasabertas.org.br>
2025-09-04 22:04:43 -0300

    update user case create

--- src/app/users/use-cases/create-user.use-case.ts
+++ src/app/users/use-cases/create-user.use-case.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+
 import { IUserRepository } from 'src/repositories/user.repository';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
@@ -13,11 +13,7 @@ export class CreateUserUseCase {
 
   async execute(createUserDto: CreateUserDto) {
     const user = new User(createUserDto);
-
-    // Criar criptografia em nossa senha
-    const salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(user.password, salt);
-
+    await user.createHashPassword(user.password);
     await this.userRepo.create(user);
     return user;
   }
